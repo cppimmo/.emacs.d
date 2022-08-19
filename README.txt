b2c407d8c87e6c9d237324fb87a590b27cb72df9
Brian Hoffpauir (cppimmo) <bhoffpauir0424@gmail.com>
2022-08-18 23:33:14 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,72 @@
+                      ____________________________
+
+                       MY GNU EMACS CONFIGURATION
+
+                            Brian Hoffpauir
+                      ____________________________
+
+
+                            [2022-08-18 Thu]
+
+
+Table of Contents
+_________________
+
+1. Note
+2. Packages Used (MELPA)
+3. License
+
+
+This repository is for versioning my GNU Emacs configuration files. I
+hope to keep them platform independent, so I can use them across all my
+machines (both UNIX-like and Windows).
+
+
+1 Note
+======
+
+  To use this configuration there should be no /~/.emacs/ file as this
+  configuration uses an /init.el/ that is stored in the /~/.emacs.d/
+  directory.  Also ensure that when cloning this repository something
+  resembling following command is used:
+  ,----
+  | mv ~/.emacs.d ~/.emacs.d.bak && git clone <url> ~/.emacs.d
+  `----
+  This configuration uses custom.el as the customize options settings
+  file.
+
+
+2 Packages Used (MELPA)
+=======================
+
+
+3 License
+=========
+
+  BSD 2-Clause License
+
+  Copyright (c) 2022, Brian Hoffpauir All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the
+     distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ init.el
@@ -0,0 +1,93 @@
+
+;; Define the customize options file. System & configuration dependent.
+;; Try to place whatever options in this file.
+(setq  custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; Try to silence annoying GPG errors, because I don't really care.
+(setq package-signature-check nil)
+
+;; Define and initialise package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+(setq inhibit-startup-message t
+	  initial-scratch-message "Welcome, Brian!"
+	  cursor-type 'bar)
+;; Set the inital frame size of Microsoft Windows. I don't use this for Linux,
+;; because I sometimes use window managers that do not supported the typical
+;; floating layout.
+(if (string-equal system-type "windows-nt")
+	(progn
+	  (when window-system (set-frame-size (selected-frame) 80 24))))
+
+;; Disable tool bar.
+(tool-bar-mode -1)
+;; Always show line cursor position.
+(column-number-mode 1)
+;; Confirmation input settings.
+;; (defalias 'yes-or-no-p 'y-or-n-p-) ; Disable for now.
+;; Set the default directory (for find-file, etc.).
+(setq default-directory user-emacs-directory)
+;; Enable line number bar globally.
+(global-display-line-numbers-mode 1)
+;; Enable overwriting of marked region.
+(delete-selection-mode t)
+;; Set the fill column in auto fill mode.
+(add-hook 'text-mode-hook '(lambda()
+							  ;; (turn-on-auto-fill) ; Keep as reference.
+							 (set-fill-column 80)))
+;; Settings for the pomodoro package.
+(use-package pomodoro)
+(require 'pomodoro)
+(pomodoro-add-to-mode-line) ; Add to the modeline.
+;; Settings for the php mode package.
+(use-package php-mode)
+;; Settings for the markdown mode package.
+(use-package markdown-mode)
+;; The the appropriate "markdown-command" for Microsoft Windows.
+(if (string-equal system-type "windows-nt")
+	(progn
+	  (custom-set-variables
+	   '(markdown-command "pandoc.exe"))))
+
+;; Install the 2048-game package.
+(use-package 2048-game)
+;; Install the doom-themes packages and set the theme.
+(use-package doom-themes
+  :config (load-theme 'doom-1337 t))
+;; Settings for the flyspell package.
+(use-package flyspell
+  :config
+  (setq ispell-program-name "hunspell.exe"
+		ispell-default-dictionary "en_US")
+  :hook (text-mode . flyspell-mode)
+  :bind (("M-<f7>" . flyspell-buffer)
+		 ("<f7>" . flyspell-word)
+		 ("C-;" . flyspell-auto-correct-previous-word)))
+
+;; Configuration for the CC mode.
+;; (setq-default whitespace-line-column 80
+;;			  whitespace-style '(face lines-tail))
+;; (add-hook 'cc-prog-mode-hook #'whitespace-mode)
+(require 'fill-column-indicator)
+
+(setq fci-rule-column 80)
+(setq fci-rule-width 2)
+(setq fci-rule-color "red")
+
+(add-hook 'cc-mode-hook 'fci-mode)
+(setq c-default-style "bsd")
+(setq-default c-basic-offset 4
+	      tab-width 4
+	      indent-tabs-mode t)
+
